6496136c6782c02f2c84a24ca764177410d831f6
Gabriel Costa <gabe.c.f.m.costa@gmail.com>
2026-02-01 02:12:47 +0000

    second iteration

--- processing/region_resolver.py
+++
@@ -1,9 +0,0 @@
-REGION_MAP = {
-    "manchester": "UK_NORTH_WEST",
-    "leeds": "UK_YORKSHIRE"
-}
-
-def resolve_region(location: str):
-    if not location:
-        return None
-    return REGION_MAP.get(location.lower())
